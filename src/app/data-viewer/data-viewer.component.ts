81c58fa5c9e3d2d1a0c978883c9d6f03d0f1831f
Kilian <kilian@cashflowpositif.fr>
2024-04-25 16:44:16 +0200

    exercice

---
+++ src/app/data-viewer/data-viewer.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-data-viewer',
+  templateUrl: './data-viewer.component.html',
+  styleUrls: ['./data-viewer.component.css']
+})
+export class DataViewerComponent implements OnInit {
+
+  data: any;
+  filteredData: any[];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.dataService.getData().subscribe(response => {
+      this.data = response;
+      this.filteredData = this.data.users.map(user => ({
+        id: user.id,
+        name: `${user.firstName} ${user.lastName}`,
+        password: user.password
+      }));
+    });
+  }
+
+  downloadJson() {
+    const fileName = "data.json";
+    const json = JSON.stringify(this.filteredData);
+    const blob = new Blob([json], { type: 'application/json' });
+    const href = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = href;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(href);
+  }
+}
